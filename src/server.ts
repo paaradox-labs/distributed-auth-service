fc33117e4b5f722c3f5d15fef2b20e0490177cf9
Mr. Paradox <avyas8927@gmail.com>
2026-01-01 13:31:08 +0530

    fix:  eslint, package json and main file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,5 @@
 function welcome(name: string) {
     console.log(`Welcome ${name}`);
-
-    const user = {
-        name: "John Doe",
-        age: 30,
-        isPaid: true,
-    };
-
-    const fname = user.name;
-    return name + fname;
 }
 
 welcome("John Doe");
