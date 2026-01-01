95d2b53932faf2f35d47b73f06fd04cef998dc01
Mr. Paradox <avyas8927@gmail.com>
2026-01-01 13:29:22 +0530

    husky fix for lint staged

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 function welcome(name: string) {
     console.log(`Welcome ${name}`);
+
+    const user = {
+        name: "John Doe",
+        age: 30,
+        isPaid: true,
+    };
+
+    const fname = user.name;
+    return name + fname;
 }
 
 welcome("John Doe");
