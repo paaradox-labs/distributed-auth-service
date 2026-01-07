90b7f3caadb56806e5530c68cc75b12559192dd5
Mr. Paradox <avyas8927@gmail.com>
2026-01-07 17:31:44 +0530

    add: test user persistent in DB

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,9 +1,28 @@
 import type { Request, Response } from "express";
+import { AppDataSource } from "../config/data-source.js";
+import { User } from "../entity/User.js";
+
+interface UserData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+interface RegisterUserRequest extends Request {
+    body: UserData;
+}
 
 export class AuthController {
-    register(req: Request, res: Response) {
-        res.status(201).json({
-            msg: "Successful",
+    async register(req: RegisterUserRequest, res: Response) {
+        const { firstName, lastName, email, password } = req.body;
+        const userRespository = AppDataSource.getRepository(User);
+        await userRespository.save({
+            firstName,
+            lastName,
+            email,
+            password,
         });
+        res.status(201).json();
     }
 }
