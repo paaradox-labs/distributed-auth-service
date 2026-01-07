1fc945cbf668ed8369e140eceec911ba2041b28d
Mr. Paradox <avyas8927@gmail.com>
2026-01-07 19:09:24 +0530

    refactor: auth layer

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import { AuthController } from "../controller/AuthController.js";
+import { UserService } from "../services/UserService.js";
+import { AppDataSource } from "../config/data-source.js";
+import { User } from "../entity/User.js";
 
 const router = express.Router();
 
-const authController = new AuthController();
+const userRepository = AppDataSource.getRepository(User);
+const userService = new UserService(userRepository);
+const authController = new AuthController(userService);
+
 router.post("/register", (req, res) => authController.register(req, res));
 
 export default router;
