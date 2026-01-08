4b46ba17be3ee178575d0ea5b926d286053a8603
Mr. Paradox <avyas8927@gmail.com>
2026-01-08 19:31:02 +0530

    refactor & logging: Auth Service & Data Persistence

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,16 +1,26 @@
 import type { Repository } from "typeorm";
 import { User } from "../entity/User.js";
 import type { UserData } from "../types/index.js";
+import createHttpError from "http-errors";
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
     async create({ firstName, lastName, email, password }: UserData) {
-        const user = await this.userRepository.save({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
-        return user;
+        try {
+            const user = await this.userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password,
+            });
+            return user;
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (err) {
+            const error = createHttpError(
+                500,
+                "Failed to store data in database",
+            );
+            throw error;
+        }
     }
 }
