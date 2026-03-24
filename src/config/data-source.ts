c982b11c5c588f0f3ab2ac4e3313cb94dade6c38
Mr. Paradox <avyas8927@gmail.com>
2026-03-24 08:35:56 +0530

    db improvements

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -5,11 +5,11 @@ import { Config } from "./index.js";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: Config.DB_HOST,
+    host: String(Config.DB_HOST),
     port: Number(Config.DB_PORT),
-    username: Config.DB_USERNAME,
-    password: Config.DB_PASSWORD,
-    database: Config.DB_NAME,
+    username: String(Config.DB_USERNAME),
+    password: String(Config.DB_PASSWORD),
+    database: String(Config.DB_NAME),
 
     // Don't use this in production
     synchronize: Config.NODE_ENV === "test" || Config.NODE_ENV === "dev",
