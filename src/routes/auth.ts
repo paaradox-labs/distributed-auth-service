f79fb88c30e7c8d7541e9aa32d1a5312b57f940e
Mr. Paradox <avyas8927@gmail.com>
2026-03-25 18:39:01 +0530

    email field validation with test

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import type { Request, Response, NextFunction } from "express";
 import { AuthController } from "../controller/AuthController.js";
 import { UserService } from "../services/UserService.js";
 import { AppDataSource } from "../config/data-source.js";
 import { User } from "../entity/User.js";
 import logger from "../config/logger.js";
+import registerValidator from "../validators/register-validator.js";
 
 const router = express.Router();
 
@@ -11,8 +13,11 @@ const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
 const authController = new AuthController(userService, logger);
 
-router.post("/register", (req, res, next) =>
-    authController.register(req, res, next),
+router.post(
+    "/register",
+    registerValidator,
+    (req: Request, res: Response, next: NextFunction) =>
+        authController.register(req, res, next),
 );
 
 export default router;
