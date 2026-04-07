ce98cd30b6adaa74c3d95747cb5dc5931d1230c6
Mr. Paradox <avyas8927@gmail.com>
2026-04-07 16:35:47 +0530

    add: rename tables

--- src/entity/RefreshTokens.ts
+++ src/entity/RefreshTokens.ts
@@ -8,7 +8,9 @@ import {
 } from "typeorm";
 import { User } from "./User.js";
 
-@Entity()
+@Entity({
+    name: "refreshTokens",
+})
 export class RefreshToken {
     @PrimaryGeneratedColumn()
     id: number;
