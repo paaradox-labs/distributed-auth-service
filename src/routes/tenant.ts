8bf1c850ac5e4e5e77c2007001d67e5fa17e0a26
Mr. Paradox <avyas8927@gmail.com>
2026-04-08 16:42:35 +0530

    add: create tenant endpoint with tests

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import { TenantController } from "../controller/TenantController.js";
+import { TenantService } from "../services/TenantService.js";
+import { AppDataSource } from "../config/data-source.js";
+import { Tenant } from "../entity/Tenant.js";
+import logger from "../config/logger.js";
 
 const router: ReturnType<typeof express.Router> = express.Router();
 
-router.post("/", (req, res) => {
-    res.status(201).json({});
-});
+const tenantRepository = AppDataSource.getRepository(Tenant);
+const tenantService = new TenantService(tenantRepository);
+const tenantController = new TenantController(tenantService, logger);
+
+router.post("/", (req, res, next) => tenantController.create(req, res, next));
 
 export default router;
---
+++ src/services/TenantService.ts
@@ -0,0 +1,11 @@
+import type { Repository } from "typeorm";
+import type { ITenant } from "../types/index.js";
+import type { Tenant } from "../entity/Tenant.js";
+
+export class TenantService {
+    constructor(private tenantRepository: Repository<Tenant>) {}
+
+    async create(tenantData: ITenant) {
+        return await this.tenantRepository.save(tenantData);
+    }
+}
